5c588b29dc585eeaff4cc0a76bef512f3e2308cd
JosiahKunz <jdkunz@anderson.edu>
2025-09-02 23:44:48 -0500

    Update excello-lighting-injection.js

--- excello-lighting-injection.js
+++ excello-lighting-injection.js
@@ -1,6 +1,5 @@
-
 let targetPatterns=["vignette"];
-let cutoutPatterns=["lm_circle", "night_light_excello"];
+let cutoutPatterns=["lm_circle", "night_light_excello", "-cutout"];
  
 function findSpritesWithPattern(patterns) {
   let matches = [];
@@ -18,42 +17,42 @@ function findSpritesWithPattern(patterns) {
   return matches;
 }
 
-  let targetSprites = findSpritesWithPattern(targetPatterns);
-  let cutoutSprites = findSpritesWithPattern(cutoutPatterns);
-  
-  const container = new PIXI.Container();
-  container.filters = [new PIXI.Filter()];
-  
-  function addTarget(index){
-    let targetSprite = targetSprites[index];
-    targetSprite.blendMode = PIXI.BLEND_MODES.NORMAL;
-    container.addChild(targetSprite);
-  }
-  
-  function addCutout(index){
+let targetSprites = findSpritesWithPattern(targetPatterns);
+let cutoutSprites = findSpritesWithPattern(cutoutPatterns);
+
+const container = new PIXI.Container();
+container.filters = [new PIXI.Filter()];
+
+function addTarget(index){
+	let targetSprite = targetSprites[index];
+	targetSprite.blendMode = PIXI.BLEND_MODES.NORMAL;
+	container.addChild(targetSprite);
+}
+
+function addCutout(index){
 	let cutout = cutoutSprites[index];
 	cutout.blendMode = PIXI.BLEND_MODES.DST_OUT;
 	container.addChild(cutout);
-  }
-  
-  function applyBlend(){
-    if (targetSprites.length==0) return;
-	if (cutoutSprites.length==0) return;
-    
-    const originalParent = targetSprites[0].parent;
-    
-    for (let i = 0; i < targetSprites.length; i++){
-      addTarget(i);
-    }
-	
-	for (let i = 0; i < cutoutSprites.length; i++){
-      addCutout(i);
-    }
-    
-    if (originalParent) {
-      originalParent.addChild(container);
-    }
-    
-  }
-  
+}
+
+function applyBlend(){
+if (targetSprites.length==0) return;
+if (cutoutSprites.length==0) return;
+
+const originalParent = targetSprites[0].parent;
+
+for (let i = 0; i < targetSprites.length; i++){
+	addTarget(i);
+}
+
+for (let i = 0; i < cutoutSprites.length; i++){
+	addCutout(i);
+}
+
+if (originalParent) {
+	originalParent.addChild(container);
+}
+
+}
+
 applyBlend();
