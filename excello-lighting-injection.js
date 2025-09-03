d5ea5b93c72645e3a61a1e1450019b5231c6c8cf
JosiahKunz <jdkunz@anderson.edu>
2025-09-02 20:42:51 -0500

    Update excello-lighting-injection.js

--- excello-lighting-injection.js
+++ excello-lighting-injection.js
@@ -1,12 +1,27 @@
-  let targetNames = ["vignette_medium_overlay"];
-  let cutoutNames = ["spotlight_1", "spotlight_2", "spotlight_3", "spotlight_4", "spotlight_5", "spotlight_6", "spotlight_7", "spotlight_8", "pc_light_front", "pc_window_l", "pc_window_r", "pc_window_r_bleed","pc_window_l_bleed", "mart_window", "mart_light_front","mart_window_bleed","house_entrance_1", "house_entrance_2", "house_entrance_3"];
+
+let targetPattern="LTT";
+let cutoutPattern="LTC";
+
+function findSpritesWithPattern(pattern) {
+  let matches = [];
+  
+  for(let objName in game.objects["ids"]) {
+		if (objName.includes(pattern)){
+			matches.push(game.objects["ids"][objName]);
+		}
+	}
+  return matches;
+}
+
+  let targetSprites = findSpritesWithPattern(targetPattern);
+  let cutoutSprites = findSpritesWithPattern(cutoutPattern);
   
   const container = new PIXI.Container();
   container.filters = [new PIXI.Filter()];
   let applied = false;
   
   function addTarget(index){
-    let targetSprite = game.objects.get(targetNames[index]).sprite;
+    let targetSprite = targetSprites[index];
     targetSprite.blendMode = PIXI.BLEND_MODES.NORMAL;
     container.addChild(targetSprite);
   }
@@ -17,48 +32,17 @@
 	container.addChild(cutout);
   }
   
-  function loopTryApplyBlend(){
-    if (applied) return;
+  function applyBlend(){
+    if (targetSprites.length==0) return;
+	if (cutoutSprites.length==0) return;
     
-    for (let i = 0; i < targetNames.length; i++) {
-      let targetObj = game.objects.get(targetNames[i]);
-      
-      
-      if (!targetObj || !targetObj.sprite) {
-        requestAnimationFrame(loopTryApplyBlend);
-		console.log(`No object named ${targetNames[i]}`);
-        return;
-      }
-      
-      if (!targetObj.sprite._texture.baseTexture.valid) {
-		console.log(`Object named ${targetNames[i]} has no valid sprite texture`);
-        requestAnimationFrame(loopTryApplyBlend);
-        return;
-      }
-    }
-	
-	for (let i = 0; i < cutoutNames.length; i++) {
-		let cutoutObj = game.objects.get(cutoutNames[i]);
-		if (!cutoutObj || !cutoutObj.sprite) {
-		console.log(`No object named ${cutoutNames[i]}`);
-        requestAnimationFrame(loopTryApplyBlend);
-        return;
-      }
-      
-      if (!cutoutObj.sprite._texture.baseTexture.valid) {
-		console.log(`Object named ${cutoutNames[i]} has no valid sprite texture`);
-        requestAnimationFrame(loopTryApplyBlend);
-        return;
-      }
-	}
+    const originalParent = targetSprites[0].parent;
     
-    const originalParent = game.objects.get(targetNames[0]).sprite.parent;
-    
-    for (let i = 0; i < targetNames.length; i++){
+    for (let i = 0; i < targetSprites.length; i++){
       addTarget(i);
     }
 	
-	for (let i = 0; i < cutoutNames.length; i++){
+	for (let i = 0; i < cutoutSprites.length; i++){
       addCutout(i);
     }
     
@@ -66,8 +50,6 @@
       originalParent.addChild(container);
     }
     
-    applied = true;
-	console.log(`Added ${targetNames.length} targets and ${cutoutNames.length} cutouts`);
   }
   
-  loopTryApplyBlend();
+applyBlend();
