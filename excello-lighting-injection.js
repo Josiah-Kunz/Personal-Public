85113d3c81c56d9f212da244f78e36f000847061
JosiahKunz <jdkunz@anderson.edu>
2025-09-03 20:55:02 -0500

    Update excello-lighting-injection.js

--- excello-lighting-injection.js
+++ excello-lighting-injection.js
@@ -1,61 +1,60 @@
+let targetPatterns=["vignette", "overlay_"];
+let cutoutPatterns=["lm_", "-cutout"];
+ 
+function findSpritesWithPattern(patterns) {
+  let matches = [];
+  
+  for(let objName in game.objects["ids"]) {
+		let gameObject = game.objects["ids"][objName];
+		if (!gameObject || !gameObject.skin) continue;
+		for(let pattern of patterns){
+			if (gameObject.skin.includes(pattern)){
+				matches.push(gameObject.sprite);
+				break;
+			}
+		}
+	}
+  return matches;
+}
 
-    let targetPatterns = ["vignette", "overlay_"];
-    let cutoutPatterns = ["lm_", "-cutout"];
-     
-    function findSpritesWithPattern(patterns) {
-        let matches = [];
-        
-        for(let objName in game.objects["ids"]) {
-            let gameObject = game.objects["ids"][objName];
-            if (!gameObject || !gameObject.skin) continue;
-            for(let pattern of patterns){
-                if (gameObject.skin.includes(pattern)){
-                    matches.push(gameObject.sprite);
-                    break;
-                }
-            }
-        }
-        return matches;
-    }
-    
-    if (!game.excelloContainer || game.excelloContainer.destroyed) {
+let targetSprites = findSpritesWithPattern(targetPatterns);
+let cutoutSprites = findSpritesWithPattern(cutoutPatterns);
+
+if (!game.excelloContainer || game.excelloContainer.destroyed) {
         game.excelloContainer = new PIXI.Container();
         game.excelloContainer.filters = [new PIXI.Filter()];
-        console.log("Created new container");
     }
-    game.excelloContainer.removeChildren();
-    
-    function applyBlend(){
-        let targetSprites = findSpritesWithPattern(targetPatterns);
-        let cutoutSprites = findSpritesWithPattern(cutoutPatterns);
-        
-        console.log("Found target sprites:", targetSprites.length);
-        console.log("Found cutout sprites:", cutoutSprites.length);
-        
-        if (targetSprites.length == 0) return;
-        if (cutoutSprites.length == 0) return;
-        
-        for (let i = 0; i < targetSprites.length; i++){
-            let targetSprite = targetSprites[i];
-            console.log("Adding target sprite:", i, targetSprite);
-            targetSprite.blendMode = PIXI.BLEND_MODES.NORMAL;
-            game.excelloContainer.addChild(targetSprite);
-        }
-        
-        for (let i = 0; i < cutoutSprites.length; i++){
-            let cutout = cutoutSprites[i];
-            console.log("Adding cutout sprite:", i, cutout);
-            cutout.blendMode = PIXI.BLEND_MODES.DST_OUT;
-            game.excelloContainer.addChild(cutout);
-        }
-        
-        console.log("Container children after adding:", game.excelloContainer.children.length);
-        
-        const originalParent = targetSprites[0].parent;
-        if (originalParent && !game.excelloContainer.parent) {
-            originalParent.addChild(game.excelloContainer);
-            console.log("Added container to parent");
-        }
-    }
-    
-    applyBlend();
+
+function addTarget(index){
+	let targetSprite = targetSprites[index];
+	targetSprite.blendMode = PIXI.BLEND_MODES.NORMAL;
+	game.container.addChild(targetSprite);
+}
+
+function addCutout(index){
+	let cutout = cutoutSprites[index];
+	cutout.blendMode = PIXI.BLEND_MODES.DST_OUT;
+	game.container.addChild(cutout);
+}
+
+function applyBlend(){
+	if (targetSprites.length==0) return;
+	if (cutoutSprites.length==0) return;
+
+	const originalParent = targetSprites[0].parent;
+
+	for (let i = 0; i < targetSprites.length; i++){
+		addTarget(i);
+	}
+
+	for (let i = 0; i < cutoutSprites.length; i++){
+		addCutout(i);
+	}
+
+	if (originalParent) {
+		originalParent.addChild(game.container);
+	}
+
+}
+
+applyBlend();
