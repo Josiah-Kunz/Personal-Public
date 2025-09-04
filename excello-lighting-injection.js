ccf7731230ccaa496e4d77db97f72a50cd50f3fd
JosiahKunz <jdkunz@anderson.edu>
2025-09-03 21:00:33 -0500

    Update excello-lighting-injection.js

--- excello-lighting-injection.js
+++ excello-lighting-injection.js
@@ -16,46 +16,39 @@ function findSpritesWithPattern(patterns) {
 	}
   return matches;
 }
-
 let targetSprites = findSpritesWithPattern(targetPatterns);
 let cutoutSprites = findSpritesWithPattern(cutoutPatterns);
-
 if (!game.excelloContainer || game.excelloContainer.destroyed) {
 	console.log("Creating container");
 	game.excelloContainer = new PIXI.Container();
 	game.excelloContainer.filters = [new PIXI.Filter()];
 }
-
 function addTarget(index){
 	let targetSprite = targetSprites[index];
-	targetSprite.blendMode = PIXI.BLEND_MODES.NORMAL;
-	game.excelloContainer.addChild(targetSprite);
+	if (targetSprite.parent !== game.excelloContainer) {
+		targetSprite.blendMode = PIXI.BLEND_MODES.NORMAL;
+		game.excelloContainer.addChild(targetSprite);
+	}
 }
-
 function addCutout(index){
 	let cutout = cutoutSprites[index];
-	cutout.blendMode = PIXI.BLEND_MODES.DST_OUT;
-	game.excelloContainer.addChild(cutout);
+	if (cutout.parent !== game.excelloContainer) {
+		cutout.blendMode = PIXI.BLEND_MODES.DST_OUT;
+		game.excelloContainer.addChild(cutout);
+	}
 }
-
 function applyBlend(){
 	if (targetSprites.length==0) return;
 	if (cutoutSprites.length==0) return;
-
 	const originalParent = targetSprites[0].parent;
-
 	for (let i = 0; i < targetSprites.length; i++){
 		addTarget(i);
 	}
-
 	for (let i = 0; i < cutoutSprites.length; i++){
 		addCutout(i);
 	}
-
-	if (originalParent) {
+	if (originalParent && game.excelloContainer.parent !== originalParent) {
 		originalParent.addChild(game.excelloContainer);
 	}
-
 }
-
 applyBlend();
