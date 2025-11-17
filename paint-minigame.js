71562be604fbbbd7e2ced7654a08173376dec064
Josiah D. Kunz <kunz.josiah@gmail.com>
2025-11-17 08:54:19 -0600

    Update paint-minigame.js

--- paint-minigame.js
+++ paint-minigame.js
@@ -599,7 +599,7 @@ function paintingGame(game, config) {
       gap: "4px",
       alignItems: "center",
       flexWrap: "wrap",
-      maxWidth: "90%"
+      maxWidth: "100%"
     });
 
     if (showBrushSizePicker) {
