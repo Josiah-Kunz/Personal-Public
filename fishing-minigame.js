95357f143ccd6d5323aac75fc4433f25af73698a
Josiah D. Kunz <kunz.josiah@gmail.com>
2026-02-01 09:46:45 -0600

    Update fishing-minigame.js

--- fishing-minigame.js
+++ fishing-minigame.js
@@ -491,7 +491,7 @@ function updateRingFillGraphic(progress){
 
 	/* Now position is relative to parent (hookRing), so use 0,0 as center */
 	const centerX = 0;
-	const centerY = -17;
+	const centerY = -16;
 
 	/* Ring dimensions */
 	const width = 54;
