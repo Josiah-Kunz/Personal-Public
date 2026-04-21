5fe410fca44a175c9124dedc75b935313eb846c5
Josiah D. Kunz <kunz.josiah@gmail.com>
2026-04-21 15:37:19 -0500

    Update OceanRenderer.js

--- SkyRenderer/OceanRenderer.js
+++ SkyRenderer/OceanRenderer.js
@@ -36,6 +36,7 @@ window.OceanRenderer = class OceanRenderer {
 			attribute vec2 aVertexPosition;
 			attribute vec2 aTextureCoord;
 			uniform mat3 projectionMatrix;
+			uniform mat3 translationMatrix;
 			uniform vec2 uResolution;
 			
 			varying vec2 vUv;
@@ -44,7 +45,7 @@ window.OceanRenderer = class OceanRenderer {
 			void main() {
 				vUv = aTextureCoord;
 				vPixelCoord = aTextureCoord * uResolution;
-				gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
+				gl_Position = vec4((projectionMatrix * translationMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
 			}
 		`;
 
