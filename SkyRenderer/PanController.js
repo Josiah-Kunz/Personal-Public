7a11a01113613e1606952ae9b1cd20f3dd97222e
Josiah D. Kunz <kunz.josiah@gmail.com>
2026-04-22 19:28:12 -0500

    Update PanController.js

--- SkyRenderer/PanController.js
+++ SkyRenderer/PanController.js
@@ -1,7 +1,5 @@
 window.PanController = class PanController {
-    
-    static GAME_BASE_HEIGHT = 288;
-    
+
     constructor(game, config = {}) {
         this.game = game;
 
@@ -9,8 +7,6 @@ window.PanController = class PanController {
             zoneTop: 464,
             zoneBottom: 592,
             panAmount: 96,
-            heightWhenPanned: 356,
-            destroyDuration: 200,
             ...config
         };
 
@@ -33,12 +29,10 @@ window.PanController = class PanController {
         const { zoneTop, zoneBottom, panAmount } = this.config;
 
         if (playerY <= zoneBottom) {
-            // In zone or above - interpolate
             const clampedY = Math.max(zoneTop, playerY);
             const t = 1 - (clampedY - zoneTop) / (zoneBottom - zoneTop);
             this.panTo(-panAmount * t);
         } else {
-            // Below zone
             this.panTo(0);
         }
     }
@@ -50,40 +44,10 @@ window.PanController = class PanController {
         const camera = this.game.camera;
         camera.offset.y = targetY;
         camera.targetX = -1;
-
-        const panRatio = Math.abs(targetY) / this.config.panAmount;
-        const h0 = PanController.GAME_BASE_HEIGHT;
-        const dh = this.config.heightWhenPanned - h0;
-        this.setScreenSize(h0 + Math.round(dh * panRatio));
-    }
-    
-    setScreenSize(height){
-        this.game.widgets.list.game.canSave = false;
-        this.game.height = height;
-        this.game.setZoom(this.game.settings.zoom);
     }
 
     destroy() {
-        const camera = this.game.camera;
-        const startOffsetY = camera.offset.y;
-        const startHeight = this.game.height;
-        const targetHeight = PanController.GAME_BASE_HEIGHT;
-        const duration = this.config.destroyDuration;
-        const startTime = performance.now();
-
-        const animate = (now) => {
-            const elapsed = now - startTime;
-            const t = Math.min(elapsed / duration, 1);
-
-            camera.offset.y = startOffsetY * (1 - t);
-            camera.targetX = -1;
-            this.setScreenSize(Math.round(startHeight + (targetHeight - startHeight) * t)); 
-
-            if (t < 1) {
-                requestAnimationFrame(animate);
-            }
-        };
-
-        requestAnimationFrame(animate);
-    } 
+        this.game.camera.offset.y = 0;
+        this.game.camera.targetX = -1;
+    }
 }
