34b8e967c3bf0e2161da96450041b82ee7adf402
Josiah D. Kunz <kunz.josiah@gmail.com>
2026-04-22 19:46:36 -0500

    Update sky-renderer.js

--- sky-renderer.js
+++ sky-renderer.js
@@ -4,7 +4,7 @@ const skyURLs = [
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/07ff4baf9f4a57674e8a61fba33e7c606c2b3ba1/SkyRenderer/SkyGradient.js",
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/094d100ff277a26992787584b8468ce31b8a270b/SkyRenderer/CelestialRenderer.js",
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/b1b17c5b59cd8b17544f0c6a46650760b37c8c86/SkyRenderer/StarRenderer.js",
-	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/7abd666b03a084af8a32e69d4824e78925f94aa2/SkyRenderer/OceanRenderer.js",
+	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/6b467f3cdae6cd1bf4849ba4fc606f225b007b50/SkyRenderer/OceanRenderer.js",
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/a2dbc3887dda2f7a42d7318dbf7924d61511f003/SkyRenderer/PanController.js",
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/7abd666b03a084af8a32e69d4824e78925f94aa2/SkyRenderer/SkyRenderer.js",
 ];
