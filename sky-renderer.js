d2820396c67bc6a2ad5ee4dc2cf6966df56abaa8
Josiah D. Kunz <kunz.josiah@gmail.com>
2026-05-05 18:32:08 -0500

    Update sky-renderer.js

--- sky-renderer.js
+++ sky-renderer.js
@@ -6,7 +6,7 @@ const skyURLs = [
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/b1b17c5b59cd8b17544f0c6a46650760b37c8c86/SkyRenderer/StarRenderer.js",
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/c7e594e3f77aee64b456b76166fcec2592c6284f/SkyRenderer/OceanRenderer.js",
 	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/a2dbc3887dda2f7a42d7318dbf7924d61511f003/SkyRenderer/PanController.js",
-	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/ae2a0e63cbae9579a932be39f3de41e4848b727c/SkyRenderer/SkyRenderer.js",
+	"https://raw.githubusercontent.com/Josiah-Kunz/Personal-Public/25324addab0e2ec8c139af6a8d1f878a94427e84/SkyRenderer/SkyRenderer.js",
 ];
  
 if (game.map && game.skyRenderer?.mapID !== game.map.id) {
